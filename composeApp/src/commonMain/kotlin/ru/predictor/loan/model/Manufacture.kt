d92c6d11830d5390336001034ef72ec9a0246805
Rinat2050 <sssr2050@yandex.ru>
2024-12-07 10:11:28 +0300

    Исправил иконку дерева на первом уровне

--- composeApp/src/commonMain/kotlin/ru/predictor/loan/model/Manufacture.kt
+++ composeApp/src/commonMain/kotlin/ru/predictor/loan/model/Manufacture.kt
@@ -1,6 +1,7 @@
 package ru.predictor.loan.model
 
 import loaninterest.composeapp.generated.resources.Res
+import loaninterest.composeapp.generated.resources.forest
 import loaninterest.composeapp.generated.resources.forge
 import loaninterest.composeapp.generated.resources.manuf
 
@@ -17,7 +18,7 @@ class Manufacture(
     var showSalary by MutableStateDelegate(false)
     var salary by MutableStateDelegate(0)
 
-    private val forestIcon = Res.drawable.manuf
+    private val forestIcon = Res.drawable.forest
     private val forgeIcon = Res.drawable.forge
     private val manufactureIcon = Res.drawable.manuf
     
